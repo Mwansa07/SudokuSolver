0e4f096601b637a3aafa60768398e0dd8d26cd9b
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-01 21:26:32 +0200

    feat: create SudokuMap data object and SudokuMapper class

---
+++ SudokuSolver/Workers/SudokuMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SudokuSolver.Data;
+
+namespace SudokuSolver.Workers
+{
+    public class SudokuMapper
+    {
+        public SudokuMap Find(int givenRow, int givenCol)
+        {
+            SudokuMap sudokuMap = new SudokuMap();
+
+            return sudokuMap;
+        }
+    }
+}
